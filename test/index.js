fb6ea06f30d85495767be6c6a9a9d046eb705ff2
Luke Edwards <luke.edwards05@gmail.com>
2017-06-19 13:42:29 -0700

    add tests

--- test/index.js
+++ test/index.js
@@ -1,26 +1,40 @@
+'use strict';
+
 const join = require('path').join;
 const Taskr = require('taskr');
 const test = require('tape');
 
 const dir = join(__dirname, 'fixtures');
-const plugins = [require('fly-clear'), require('../')];
+const tmp = join(__dirname, 'tmp');
 
-const tmpDir = str => join(__dirname, str);
-const create = tasks => new Taskr({ tasks, plugins });
+test('taskr-autoprefixer', t => {
+	t.plan(5);
 
-test('taskr-<%= pluginSlugName %>', t => {
-	t.plan(3);
-	const taskr = create({
-		*foo(task) {
-			t.true('<%= pluginSlugName %>' in task, 'attach `<%= pluginSlugName %>` to Task instance');
-			t.true('<%= pluginSlugName %>' in taskr.plugins, 'attach `<%= pluginSlugName %>` plugin to instance');
-			// example usage test
-			const tmp = tmpDir('tmp1');
-			yield f.source(`${dir}/*.js`).target(tmp);
-			const arr = yield f.$.expand(`${tmp}/*.js`);
-			t.equal(arr.length, 1, 'copied one file to target tar');
-			yield f.clear(tmp); // cleanup
+	const taskr = new Taskr({
+		plugins: [
+			require('../'),
+			require('@taskr/clear')
+		],
+		tasks: {
+			* foo(f) {
+				yield f.source(`${dir}/foo.css`).autoprefixer().target(tmp);
+				t.ok('autoprefixer' in taskr.plugins, 'attach `autoprefixer()` plugin to Taskr');
+				const str = yield f.$.read(`${tmp}/foo.css`, 'utf8');
+				t.true(/-webkit-/i.test(str), 'add appropriate vendor prefixes');
+				yield f.clear(tmp);
+			},
+			*bar(f) {
+				yield f.source(`${dir}/*.css`).autoprefixer().target(tmp);
+				const arr = yield f.$.expand(`${tmp}/*.css`);
+				t.equal(arr.length, 2, 'sends both files to target');
+				const str1 = yield f.$.read(`${tmp}/foo.css`, 'utf8');
+				t.true(/-webkit-/i.test(str1), 'prefixes first file');
+				const str2 = yield f.$.read(`${tmp}/bar.css`, 'utf8');
+				t.true(/-ms-flexbox/i.test(str2), 'prefixes second file');
+				yield f.clear(tmp);
+			}
 		}
 	});
-	taskr.start('foo');
+
+	taskr.serial(['foo', 'bar']);
 });
